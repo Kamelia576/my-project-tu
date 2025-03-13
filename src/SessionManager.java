5b1f8972eed600faf49ef9cebaab94e8fb8dd861
Kamelia576 <137693505+Kamelia576@users.noreply.github.com>
2025-03-13 15:58:53 +0200

    Added SessionManager class

---
+++ src/SessionManager.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessionManager {
+    private int sessionId = 1;
+    private List<ImageProcessor> images = new ArrayList<>();
+
+    public void loadImage(String filename) {
+        ImageProcessor image = new ImageProcessor(filename);
+        if (image.load()) {
+            images.add(image);
+            System.out.println("Session with ID: " + sessionId + " started");
+            System.out.println("Image \"" + filename + "\" added");
+        } else {
+            System.out.println("Error: Failed to load image " + filename);
+        }
+    }
+
+    public void printSessionInfo() {
+        System.out.println("Session ID: " + sessionId);
+        System.out.print("Loaded images: ");
+        for (ImageProcessor img : images) {
+            System.out.print(img.getFilename() + " ");
+        }
+        System.out.println();
+    }
+}
